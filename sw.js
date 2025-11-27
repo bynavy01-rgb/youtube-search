97e362592f155ac7d0a666b8bf9caefdfea529c7
bynavy01-rgb <bynavy01@gmail.com>
2025-11-28 02:21:56 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// Service Worker - PWA 오프라인 지원
+const CACHE_NAME = 'youtube-analyzer-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// 설치 이벤트
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 활성화 이벤트
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch 이벤트
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
